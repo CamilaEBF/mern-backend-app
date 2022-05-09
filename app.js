d6f3f264dd576cb4f1e0fb21ea37ac498d40ec39
Camila Blanc Fick <camilaebf@gmail.com>
2022-05-08 22:06:05 -0300

    implementing get

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 
 const app = express();
-
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    const response = {hello: 'This is my API'};
+    res.status(200).json(response);
+  });
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');

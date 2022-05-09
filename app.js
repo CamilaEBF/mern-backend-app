5d30be99ad97c11a023009dd45cfb4c55b0c072e
Camila Blanc Fick <camilaebf@gmail.com>
2022-05-09 01:46:49 -0300

    wip add database connection mongo db

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const db = mongoose.connect('mongodb://localhost:27017/mern');
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
 
 
 bookRouter.route('/books')
